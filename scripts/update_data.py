1cb9e36c32be8bf1288e8bb4462fd809bf8c7feb
kevinnto <kevinn.to01@gmail.com>
2026-06-20 21:16:11 +0200

    new update file

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -36,7 +36,7 @@ ALIASES = {
     "unitedstates": "usa", "unitedstatesofamerica": "usa",
     "ivorycoast": "cotedivoire",
     "drcongo": "congodr", "democraticrepublicofthecongo": "congodr", "dccongo": "congodr",
-    "capeverde": "caboverde", "capeverdeisland": "caboverde", "capeverdeisland": "caboverde", 
+    "capeverde": "caboverde",
     "bosniaandherzegovina": "bosnia", "bosniaherzegovina": "bosnia", "bosniahercegovina": "bosnia",
     "czechrepublic": "czechia",
 }
